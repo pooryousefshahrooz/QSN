047a2a52d86460ff1338d977573fc515e6701a2e
Shahrooz Pooryousef Delzei <sh.pooryousef2@gmail.com>
2024-03-18 20:15:25 -0400

    qubit decoherence assumptions on storage

--- solver.py
+++ solver.py
@@ -199,10 +199,12 @@ class Solver:
                 for b in network.each_storage_blocks[j]:
                     sum_served_from_storage = 0
                     for k in work_load.each_t_user_pairs[t]:
-                        if k!=j:
-                            for p in network.each_request_each_storage_each_block_paths[k][j][b]:
-                                sum_served_from_storage +=w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
-                    
+                        
+                        for p in network.each_request_each_storage_each_block_paths[k][j][b]:
+                            sum_served_from_storage +=w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
+                            if sum_served_from_storage>0:
+                                print("virtual path %s has rate %s with distillation %s "%(p,w_vars[t,k,p].solution_value,
+                                                                                      w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))))
                     if sum_served_from_storage>0:
                         print("served from storage %s cannot be higher than %s "%(sum_served_from_storage*network.delta_value,u_vars[j,b,t].solution_value))
         time.sleep(3)
