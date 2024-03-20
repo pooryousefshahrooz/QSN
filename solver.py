21691aab04eeb5067d30efd5bc35f8b8c63f5c2e
Shahrooz Pooryousef Delzei <sh.pooryousef2@gmail.com>
2024-03-19 23:48:29 -0400

    qubit decoherence assumptions on storage

--- solver.py
+++ solver.py
@@ -32,8 +32,8 @@ class Solver:
         w_vars = {}
         u_vars = {}
         
-        w_vars  = {(t,k,p): opt_model.continuous_var(lb=0, ub= network.max_edge_capacity,
-                                  name="w_{0}_{1}_{2}".format(t,k,p))  for t in work_load.T 
+        w_vars  = {(k,p,t): opt_model.continuous_var(lb=0, ub= network.max_edge_capacity,
+                                  name="w_{0}_{1}_{2}".format(k,p,t))  for t in work_load.T 
                    for k in work_load.each_t_user_pairs[t]+network.storage_pairs 
                    for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k]}
 #         print("w_vars",w_vars)
@@ -53,11 +53,11 @@ class Solver:
 #                         for p_s in network.each_request_real_paths[j,b]:
                         if cyclic_workload:
                             opt_model.add_constraint(u_vars[j,b,t] == u_vars[j,b,(t-1)%len(work_load.T)]/network.get_each_storage_block_freshness(j,b)-
-                            opt_model.sum(w_vars[(t-1)%len(work_load.T),k,p] *
+                            opt_model.sum(w_vars[k,p,(t-1)%len(work_load.T)] *
                             network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
                             for k in work_load.each_t_requests[t] if k!=j 
                             for p in network.each_request_each_storage_each_block_paths[k][j][b])*network.delta_value
-                            +opt_model.sum(w_vars[(t-1)%len(work_load.T),j,p2] for p2 in network.each_storage_block_paths[j][b])*network.delta_value
+                            +opt_model.sum(w_vars[j,p2,(t-1)%len(work_load.T)] for p2 in network.each_storage_block_paths[j][b])*network.delta_value
                                                  ,ctname="inventory_evolution_{0}_{1}_{2}".format(t,j,b))
 #                         else:
 #                             opt_model.add_constraint(u_vars[t,j,b,p_s] == u_vars[t-1,j,b,p_s]/network.get_each_storage_block_freshness(j,b)-
@@ -76,28 +76,28 @@ class Solver:
 
                             if cyclic_workload:
                                 opt_model.add_constraint(u_vars[j,b,t] == -
-                                opt_model.sum(w_vars[(t-1)%len(work_load.T),k,p] *
+                                opt_model.sum(w_vars[k,p,(t-1)%len(work_load.T)] *
                                 network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
                                 for k in work_load.each_t_requests[t] if k!=j 
                                 for p in network.each_request_each_storage_each_block_paths[k][j][b] 
                                 )*network.delta_value
-                                + opt_model.sum(w_vars[(t-1)%len(work_load.T),j,p_s])*network.delta_value
+                                + opt_model.sum(w_vars[j,p_s,(t-1)%len(work_load.T)])*network.delta_value
                                                      , ctname="inventory_evolution_{0}_{1}".format(t,j,p_s))
                             else:
                                 opt_model.add_constraint(u_vars[j,b,t] == -
-                                opt_model.sum(w_vars[t-1,k,p] *
+                                opt_model.sum(w_vars[k,p,t-1] *
                                 network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
                                 for k in work_load.each_t_requests[t] if k!=j 
                                 for p in network.each_request_each_storage_each_block_paths[k][j][b] 
                                 )*network.delta_value
-                                + opt_model.sum(w_vars[t-1,j,p_s])*network.delta_value
+                                + opt_model.sum(w_vars[j,p_s,t-1])*network.delta_value
                                                      , ctname="inventory_evolution_{0}_{1}".format(t,j,p_s))
 
         # serving from inventory constraint
         for t in work_load.T[1:]:
             for j in network.storage_pairs:
                 for b in network.each_storage_blocks[j]:
-                    opt_model.add_constraint(opt_model.sum(w_vars[t,k,p]*
+                    opt_model.add_constraint(opt_model.sum(w_vars[k,p,t]*
                     network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
                     for k in work_load.each_t_user_pairs[t] if k!=j 
                     for p in network.each_request_each_storage_each_block_paths[k][j][b]
@@ -109,7 +109,7 @@ class Solver:
             # Demand constriant
             for t in work_load.T[1:]:
                 for k in  work_load.each_t_user_pairs[t]:
-                    opt_model.add_constraint(opt_model.sum(w_vars[t,k,p]
+                    opt_model.add_constraint(opt_model.sum(w_vars[k,p,t]
                     for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k]) >= 
                             work_load.each_t_each_request_demand[t][k], ctname="constraint_{0}_{1}".format(t,k))
 
@@ -117,7 +117,7 @@ class Solver:
         for t in work_load.T:
             for edge in network.set_E:
                 opt_model.add_constraint(
-                    opt_model.sum(w_vars[t,k,p]*
+                    opt_model.sum(w_vars[k,p,t]*
                     network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t)) 
                     for k in work_load.each_t_user_pairs[t]+network.storage_pairs
                     for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k] if network.check_path_include_edge(edge,p))
@@ -159,13 +159,13 @@ class Solver:
         if feasibility_flag:
             objective = opt_model.sum(1/len(work_load.T[1:])*1/len(work_load.each_t_real_requests[t])
                                       *1/work_load.each_t_each_request_demand[t][k]
-                                      *(w_vars[t,k,p] * network.get_path_length(p)) for t in work_load.T[1:]
+                                      *(w_vars[k,p,t] * network.get_path_length(p)) for t in work_load.T[1:]
                                       for k in work_load.each_t_user_pairs[t] 
                                       for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k]
                                       )
             opt_model.minimize(objective)
         else:
-            objective = opt_model.sum((w_vars[t,k,p]) for t in work_load.T[-1:]
+            objective = opt_model.sum((w_vars[k,p,t]) for t in work_load.T[-1:]
                                       for k in work_load.each_t_user_pairs[t] 
                                       for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k]
                                       )
@@ -184,7 +184,7 @@ class Solver:
                                                                                  network.each_path_basic_fidelity[p],
                                                                                  network.oracle_for_target_fidelity[p][work_load.get_each_request_threshold(network,k,10,t)],
                                                                                  network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t)),
-                                                                                 w_vars[t,k,p].solution_value))
+                                                                                 w_vars[k,p,t].solution_value))
 
                     
         for t in work_load.T[-1:]:
@@ -193,8 +193,8 @@ class Solver:
                 for k in work_load.each_t_user_pairs[t]:
                     for p in network.each_request_real_paths[k]+network.each_request_virtual_paths[k]:
                         if network.check_path_include_edge(edge,p):
-                            edge_sum+= w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t))
-                            print("for path %s rate is %s distillation %s product %s "%(p,w_vars[t,k,p].solution_value,network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t)),w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t))))
+                            edge_sum+= w_vars[k,p,t].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t))
+                            print("for path %s rate is %s distillation %s product %s "%(p,w_vars[k,p,t].solution_value,network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t)),w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,10,t))))
                 
 
                 print("for edge %s we have load %s its capacity is %s "%(edge,edge_sum,network.each_edge_capacity[edge]))
@@ -208,11 +208,11 @@ class Solver:
                     for k in work_load.each_t_user_pairs[t]:
                         
                         for p in network.each_request_each_storage_each_block_paths[k][j][b]:
-                            sum_served_from_storage +=w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
+                            sum_served_from_storage +=w_vars[k,p,t].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))
                             if sum_served_from_storage>0:
-                                print("virtual path %s has rate %s g() %s with distillation %s "%(p,w_vars[t,k,p].solution_value,
+                                print("virtual path %s has rate %s g() %s with distillation %s "%(p,w_vars[k,p,t].solution_value,
                                                                                                   network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t)),
-                                                                                      w_vars[t,k,p].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))))
+                                                                                      w_vars[k,p,t].solution_value* network.get_required_purification_EPR_pairs(p,work_load.get_each_request_threshold(network,k,b,t))))
                     if sum_served_from_storage>0:
                         print("served from storage %s cannot be higher than %s with fr() = %s "%(sum_served_from_storage*network.delta_value,
                                                                                                  u_vars[j,b,t].solution_value,
